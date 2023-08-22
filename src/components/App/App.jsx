3cd47d55d3e3775c6ad5313e22c6ce76aa6dd115
MaksymBora <maxboraod@gmail.com>
2023-08-22 22:24:17 +0200

    refactored app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,3 @@
-import React, { Component } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { fetchImages } from 'API';
 import { Searchbar } from '../Searchbar/Searchbar';
@@ -6,92 +5,74 @@ import { Gallery } from '../ImageGallery/ImageGallery';
 import { Pagination } from '../LoadMore/LoadMore';
 import { Wrapper } from './App.styled'
 import { Loader } from '../Loader/Loader'
-import {notifyInfo, notifyInputQuerry, success} from '../Notify/notify'
+import { notifyInfo, notifyInputQuerry, success } from '../Notify/notify'
+const { useState, useEffect } = require("react");
 
 
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
 
-export class App extends Component {
-  state = {
-    query: '',
-    images: [],
-    page: 1,
-    loading: false,
+  const changeQuery = newQuery => {
+    setQuery(`${Date.now()}/${newQuery}`);
+    setImages([]);
+    setPage(1);
   };
 
+  useEffect(() => {
+    if (query === '') return;
 
-  changeQuery = newQuery => {
-    this.setState({
-      query: `${Date.now()}/${newQuery}`,
-      images: [],
-      page: 1
-    });
-    
-  };
+    const loadResult = async () => {
+      try {
+        setLoading(true);
+        const img = await fetchImages(query, page);
 
-  componentDidUpdate = async (prevProps, prevState) => {
-    const prevQuery = prevState.query;
-    const searchQuery = this.state.query;
-    const prevPage = prevState.page;
-    const nexPage = this.state.page;
-
-    if (prevQuery !== searchQuery || prevPage !== nexPage) {
-      this.loadResult();
-    }
-  };
-
-  loadResult = async () => {
-    const searchQuery = this.state.query;
-    const nexPage = this.state.page;
-
-    try {
-      this.setState({ loading: true });
-      const img = await fetchImages(searchQuery, nexPage);
-      if (img.length) {
-        this.setState(prevState => ({
-          images: this.state.page > 1 ? [...prevState.images, ...img] : img,
-        }));
-        success(searchQuery);
-        this.setState({ loading: false });
-      } else {
-        notifyInfo();
-        this.setState({ loading: false });
+        if (img.length) {
+          setImages(prevState => (page > 1 ? [...prevState, ...img] : img));
+          success(query);
+          setLoading(false);
+        } else {
+          notifyInfo();
+          setLoading(false);
+        }
+      } catch (error) {
+        console.log(error);
+        
+      } finally {
+        setLoading(false);
       }
-    } catch (error) {
-      console.log(error);
-      this.setState({ loading: false });
-    }
-  };
+    };
+    
+    loadResult();
+  }, [page, query]);
 
-  handleSubmit = (evt) => {
+  const handleSubmit = (evt) => {
     evt.preventDefault();
     if (evt.target.elements.query.value.trim() === '') {
       notifyInputQuerry();
       return;
     }
-    this.changeQuery(evt.target.elements.query.value);
 
+    changeQuery(evt.target.elements.query.value);
 
     evt.target.reset();
   };
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const handleLoadMore = () => {
+    setPage(page + 1);
   };
-  
 
-  render () {
-    const { loading, images } = this.state;
     return (
       <Wrapper>
-        <Searchbar onSubmit={ this.handleSubmit } />
+        <Searchbar onSubmit={ handleSubmit } />
         { loading && <Loader /> }
         { images.length > 0 && <Gallery imgItems={ images } /> } 
-        { images.length > 0 && <Pagination onClick={ this.handleLoadMore }>Load More</Pagination> }
+        { images.length > 0 && <Pagination onClick={ handleLoadMore }>Load More</Pagination> }
         <Toaster position="top-right" reverseOrder={true}/>
       </Wrapper>
     )
-  }
-};
+
+}
